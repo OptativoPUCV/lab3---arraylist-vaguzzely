1b36828d9bf738ed15d32d991044e2e8fa1d62c6
Replit user <>
2023-03-31 17:42:52 +0000

    new try

--- arraylist.c
+++ arraylist.c
@@ -48,7 +48,16 @@ void* get(ArrayList * l, int i){
   if (i >= l->size) {
     return NULL;
   }
+  if (i < 0) {
+    i = l->size + i;
+  }
+  ArrayList* current = l->first;
+  for (int j = 0; j < i; j++) {
+    current = current->next;
+  }
     return NULL;
+
+  return current->data;
 }
 
 int get_size(ArrayList * l){
